e9f4c05f004fcd7b3221dd63e65c1b5320c0f50a
michele <michele.gaiarin@gmail.com>
2019-12-17 17:21:57 +0000

    Modify thread with final version

--- sonar/main.py
+++ sonar/main.py
@@ -17,11 +17,9 @@ class Mapping(QtCore.QThread):
 
     def __init__(self):
         super().__init__()
-        self.runs = True
 
     def run( self ):
         self.signalMapping.emit()
-    
 
 class Sonar(QMainWindow):
     def __init__(self):
@@ -29,8 +27,15 @@ class Sonar(QMainWindow):
         self.ser = None
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.theta = []
+        self.r = []
 
-        
+        self.map = Mapping()
+        self.mapThread = QtCore.QThread()
+        self.mapThread.started.connect(self.map.run)
+        self.map.signalMapping.connect(self.mapping)
+        self.map.moveToThread(self.mapThread)
+
         # Signal/Slot connections
         self.ui.btnConnection.clicked.connect(self.onConnectionClicked)
         self.ui.btnMapping.clicked.connect(self.onMappingClicked)
@@ -39,24 +44,17 @@ class Sonar(QMainWindow):
         try:
             #Open port at 9600,8,N,1 no timeout
             #time.sleep(1)
-            self.ser = serial.Serial('/dev/ttyUSB0')
+            self.ser = serial.Serial('/dev/ttyUSB1')
         except:
             self.ui.lbConnectionStatus.setText("Connection error!!\nUnable to connect with ATMEGA328P")
             print("Connection error. Unable to connect with ATMEGA328P")
-            self.ui.btnMapping.setEnabled(True)
         else:
             self.ui.lbConnectionStatus.setText("Connesione stabilita!")
             self.ui.btnMapping.setEnabled(True)
 
     def onMappingClicked(self):
+        self.ui.btnConnection.setEnabled(False)
         self.ui.btnMapping.setEnabled(False)
-
-        self.map = Mapping()
-        self.mapThread = QtCore.QThread()
-        self.mapThread.started.connect(self.map.run)
-        self.map.signalMapping.connect(self.mapping)
-        self.map.moveToThread(self.mapThread)
-
         self.mapThread.start()
 
 
@@ -64,15 +62,34 @@ class Sonar(QMainWindow):
         fig  = plot.figure()
         fig.add_subplot(111, projection='polar')
 
-        r = [100, 100, 300, 300, 300, 300, 40, 40, 20, 20, 20, 300, 57, 60, 55, 49, 100, 55, 55, 300, 300, 200, 10, 11, 13, 10, 10, 300, 300, 88, 88, 90, 10, 10, 12, 300]
-        theta = []
-        for i in range(0, len(r)):
-            theta.append(math.radians(5.625 * i))
+        self.ser.write(b'A')
+        #read line from serial port
+        line = self.ser.readline().strip()
+        coordinate = line.decode('ascii')
+        #Remove special char
+        coordinate = coordinate.replace('\r', '')
+        coordinate = coordinate.replace('\n', '')
+        while (coordinate != 'E'):
+            #Get value from the string
+            degree = re.split('[;]', coordinate)[0]
+            distance = re.split('[;]', coordinate)[1]
+            #Save and convert value on float
+            self.theta.append(math.radians(float(degree)))
+            self.r.append(float(distance))
 
-        time.sleep(2)
-        plot.polar(theta, r)
-        # Display the cardioids -
+            #read line from serial port
+            line = self.ser.readline().strip()
+            coordinate = line.decode('ascii')
+            #Remove special char
+            coordinate = coordinate.replace('\r', '')
+            coordinate = coordinate.replace('\n', '')
+            time.sleep(0.5)
+
+        plot.polar(self.theta, self.r)
+        # Display the mapping
         plot.show()
+        self.ui.btnMapping.setEnabled(True)
+        self.ui.btnConnection.setEnabled(True)
         self.mapThread.exit()
 
 if __name__ == "__main__":
